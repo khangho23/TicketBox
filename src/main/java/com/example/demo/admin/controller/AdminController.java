0bcd8dd983ebe3356854426fb20f8b95c0219d6d
unknown <hohoangkhang18@gmail.com>
2023-09-09 11:29:59 +0700

    Fix url admin section...

--- src/main/java/com/example/demo/admin/controller/AdminController.java
+++ src/main/java/com/example/demo/admin/controller/AdminController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/admin")
 public class AdminController {
-	@GetMapping("")
+	@GetMapping({"", "/dash"})
 	public String index() {
 		return "index.html";
 	}
