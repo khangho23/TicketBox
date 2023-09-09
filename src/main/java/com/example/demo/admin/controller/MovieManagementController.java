bfdc637343bc412e2154eca0840c30abef39bbbf
unknown <hohoangkhang18@gmail.com>
2023-09-09 12:02:46 +0700

    Add - icon react, url movieDetailPage and fix event.preventDefault is not function

--- src/main/java/com/example/demo/admin/controller/MovieManagementController.java
+++ src/main/java/com/example/demo/admin/controller/MovieManagementController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/zuhot/management-movie")
+@RequestMapping("/admin/management-movie")
 public class MovieManagementController {
     @GetMapping("")
     public ResponseEntity<?> index() {
