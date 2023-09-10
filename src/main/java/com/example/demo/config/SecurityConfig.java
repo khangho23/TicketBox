73ef51d5fbcdab1df97a3054a85cf8d9a0df1121
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-09-10 19:50:00 +0700

    complete config security

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,56 +17,47 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    // @Bean
-    // public InMemoryUserDetailsManager userDetailsService(PasswordEncoder
-    // passwordEncoder) {
-    // UserDetails user = User.withUsername("user")
-    // .password(passwordEncoder.encode("password"))
-    // .roles("USER")
-    // .build();
 
-    // UserDetails admin = User.withUsername("admin")
-    // .password(passwordEncoder.encode("admin"))
-    // .roles("USER", "ADMIN")
-    // .build();
-
-    // return new InMemoryUserDetailsManager(user, admin);
-    // }
-
-    // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //     http.csrf().disable().authorizeRequests()
-    //     .requestMatchers("/admin").permitAll()
-    //     .anyRequest().authenticated().and()
-
-    //             .httpBasic();
-    //     return http.build();
-    // }
     @Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/admin/zuhot/*","/admin","/admin/").permitAll()
-				.anyRequest().authenticated()
-			).formLogin((form) -> form
-            .loginPage("/login")
-            .permitAll()
-        )
-        .logout((logout) -> logout.permitAll());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/admin/*", "/admin")
+                        .authenticated()
+                        .anyRequest().permitAll())
+                .authenticationProvider(authenticationProvider())
+                .formLogin((form) -> form
+                        .loginPage("/admin/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/admin")
+                        .permitAll())
+                .logout((logout) -> logout.permitAll());
 
+        return http.build();
+    }
 
-		return http.build();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("zuhot")
+                                .password(passwordEncoder().encode("zuhot12345"))
+                                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
-		return new InMemoryUserDetailsManager(user);
-	}
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
+        dao.setUserDetailsService(userDetailsService());
+        dao.setPasswordEncoder(passwordEncoder());
+        return dao;
+    }
+
 }
