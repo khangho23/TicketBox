d3fc267c5498989dc9380472103f6900c9c2253b
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-09-16 20:42:14 +0700

    change

--- src/main/java/com/example/demo/controller/ShowTimeController.java
+++ src/main/java/com/example/demo/controller/ShowTimeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.ShowTimeService;
 
 @RestController
-@RequestMapping("/showtime")
+@RequestMapping("/api/showtime")
 @CrossOrigin("*")
 public class ShowTimeController {
 	@Autowired
