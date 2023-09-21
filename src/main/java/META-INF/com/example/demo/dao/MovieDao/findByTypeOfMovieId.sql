9e41083c3d6f9948d562d7018575caa157722ade
Bùi Vũ Thiên Ân (anbvt) <anbvt562003@gmail.com>
2023-09-21 12:34:17 +0700

    Update SQL

--- src/main/java/META-INF/com/example/demo/dao/MovieDao/findByTypeOfMovieId.sql
+++ src/main/java/META-INF/com/example/demo/dao/MovieDao/findByTypeOfMovieId.sql
@@ -3,5 +3,5 @@ SELECT movie.id,
 FROM movie 
 LEFT JOIN moviedetails ON movie.id = moviedetails.movieid
 WHERE moviedetails.typeofmovieid IN /* typeofmovieid */('LP01','LP02')
-GROUP BY movie.id
+GROUP BY movie.id, movie.poster
 LIMIT 10
