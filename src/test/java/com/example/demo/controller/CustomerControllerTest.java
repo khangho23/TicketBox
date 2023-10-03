dc7c321fbb4234241876efbbf78d99e21eebc3b2
khangho23 <hohoangkhang18@gmail.com>
2023-10-03 15:48:43 -0700

    Test NG

--- src/test/java/com/example/demo/controller/CustomerControllerTest.java
+++ src/test/java/com/example/demo/controller/CustomerControllerTest.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -31,7 +34,7 @@ public class CustomerControllerTest {
 	public void findById() throws Exception {
 		expect = gsonService.getValueExpect(this.getClass().toString(), "findById");
 		this.mockMvc.perform(get(URL + "/{id}", 1).accept(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(content().string(containsString(expect)));
+		        .andExpect(content().string(containsString(expect)));
 	}
 
 	@Test
