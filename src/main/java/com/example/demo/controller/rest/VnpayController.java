4bdefb65daf0af24681ebcf97698a0182065ee16
khangho23 <hohoangkhang18@gmail.com>
2023-10-17 03:39:48 +0700

    tam on thoi nhess

--- src/main/java/com/example/demo/controller/rest/VnpayController.java
+++ src/main/java/com/example/demo/controller/rest/VnpayController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller.rest;
 
 import com.example.demo.config.VnpayConfig;
 import com.example.demo.dto.VnpayPaymentDto;
+import com.example.demo.dto.VnpayResultDto;
 import com.example.demo.dto.VnpayToken;
 import com.example.demo.exception.InvalidRequestParameterException;
 import com.example.demo.service.VnpayService;
@@ -33,51 +34,22 @@ public class VnpayController {
         return ResponseEntity.ok(vnpayService.createPayment(request, vnp));
     }
 
-    @GetMapping("/payment-information")
-    public Map<String, String> paymentInformation(@RequestParam("vnp_OrderInfo") String vnp_OrderInfo,
-                                                  @RequestParam("vnp_Amount") Integer vnp_Amount,
-                                                  @RequestParam("vnp_BankCode") String vnp_BankCode,
-                                                  @RequestParam("vnp_BankTranNo") Optional<String> vnp_BankTranNo,
-                                                  @RequestParam("vnp_CardType") Optional<String> vnp_CardType,
-                                                  @RequestParam("vnp_PayDate") Optional<String> vnp_PayDate,
-                                                  @RequestParam("vnp_ResponseCode") String vnp_ResponseCode,
-                                                  @RequestParam("vnp_TransactionNo") String vnp_TransactionNo,
-                                                  @RequestParam("vnp_TransactionStatus") String vnp_TransactionStatus,
-                                                  @RequestParam("vnp_TxnRef") String vnp_TxnRef,
-                                                  @RequestParam("vnp_SecureHash") String vnp_SecureHash) {
-        Map<String, String> response = new HashMap<>();
-
-        String payDate = vnp_PayDate.orElse("");
-        String year = payDate.substring(0, 4);
-        String month = payDate.substring(4, 6);
-        String date = payDate.substring(6, 8);
-        String hour = payDate.substring(8, 10);
-        String minutes = payDate.substring(10, 12);
-        String second = payDate.substring(12, 14);
-
-        String timePay = date + "-" + month + "-" + year + " " + hour + ":" + minutes + ":" + second;
-
-        response.put("vnp_OrderInfo", vnp_OrderInfo);
-        response.put("vnp_Amount", vnp_Amount.toString());
-        response.put("vnp_BankCode", vnp_BankCode);
-        response.put("vnp_BankTranNo", vnp_BankTranNo.orElse(""));
-        response.put("vnp_CardType", vnp_CardType.orElse(""));
-        response.put("vnp_PayDate", timePay);
-        response.put("vnp_ResponseCode", vnp_ResponseCode);
-        response.put("vnp_TransactionNo", vnp_TransactionNo);
-        response.put("vnp_TransactionStatus", vnp_TransactionStatus);
-        response.put("vnp_TxnRef", vnp_TxnRef);
-        response.put("vnp_SecureHash", vnp_SecureHash);
-
-        return response;
+    @PostMapping("/payment-information")
+    public ResponseEntity<?> paymentInformation() throws InvalidRequestParameterException {
+        return ResponseEntity.ok(vnpayService.paymentInformation(request));
     }
 
-    @GetMapping("/create-token")
+    @PostMapping("/refund")
+    public ResponseEntity<?> refund(@RequestBody VnpayPaymentDto vnpayPaymentDto) throws InvalidRequestParameterException {
+        return ResponseEntity.ok(vnpayService.refund(request, vnpayPaymentDto));
+    }
+
+    @PostMapping("/create-token")
     public ResponseEntity<?> createToken(@RequestBody VnpayToken vnpayToken) throws InvalidRequestParameterException {
         return ResponseEntity.ok(vnpayService.createToken(request, vnpayToken));
     }
 
-    @GetMapping("/pay-and-create-token")
+    @PostMapping("/pay-and-create-token")
     public ResponseEntity<?> paymentAndCreateToken(@RequestBody VnpayToken vnpayToken) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InvalidRequestParameterException {
         return ResponseEntity.ok(vnpayService.paymentAndCreateToken(request, vnpayToken));
     }
@@ -87,7 +59,7 @@ public class VnpayController {
         return ResponseEntity.ok(vnpayService.paymentByToken(request, vnpayToken));
     }
 
-    @PostMapping("/remove-token")
+    @DeleteMapping("/remove-token")
     public ResponseEntity<?> removeToken(@RequestBody VnpayToken vnpayToken) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InvalidRequestParameterException {
         return ResponseEntity.ok(vnpayService.removeToken(request, vnpayToken));
     }
