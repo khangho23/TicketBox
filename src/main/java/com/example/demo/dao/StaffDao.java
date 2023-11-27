e8b2328247208853b00f140761411b80ee73d2a1
dkminh.hcm <dkminh.hcm@vnpt.vn>
2023-11-28 05:17:38 +0700

    add change password

--- src/main/java/com/example/demo/dao/StaffDao.java
+++ src/main/java/com/example/demo/dao/StaffDao.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Insert;
 import org.seasar.doma.Select;
+import org.seasar.doma.Update;
 import org.seasar.doma.boot.ConfigAutowireable;
 
 import com.example.demo.entity.Staff;
+import com.example.demo.model.StaffUpdatePasswordModel;
 
 @Dao
 @ConfigAutowireable
@@ -21,4 +23,10 @@ public interface StaffDao {
 
 	@Select
 	Optional<Staff> findById(String id);
+
+	@Update(sqlFile = true)
+	int update(Staff staff);
+
+	@Update(sqlFile = true)
+	int updatePassword(StaffUpdatePasswordModel staff);
 }
