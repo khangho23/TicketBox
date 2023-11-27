0241f93b00f6526ec71db04746322bdcfe961d26
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-11-28 01:34:41 +0700

    movie config

---
+++ src/main/java/META-INF/com/example/demo/dao/MovieConfigDao/update.sql
@@ -0,0 +1,14 @@
+UPDATE movieconfig 
+SET 
+startdate = /* movieConfig.startDate */'0',
+enddate = /* movieConfig.endDate */'0',
+createdate = /* movieConfig.createDate */'0',
+updatedate = /* movieConfig.updateDate */'0',
+status = 
+        CASE
+            WHEN CURRENT_DATE BETWEEN /* movieConfig.startDate */'0' AND /* movieConfig.endDate */'0' THEN 1
+            WHEN CURRENT_DATE > /* movieConfig.endDate */'0' THEN 2
+            WHEN CURRENT_DATE < /* movieConfig.startDate */'0' THEN 0
+            ELSE null
+        END
+WHERE movieid =/*movieConfig.movieId*/'0' and branchid = /*movieConfig.branchId*/'0'
---
+++ src/main/java/com/example/demo/service/MovieConfigService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.admin.controller.enums.RequestParameterEnum;
+import com.example.demo.admin.controller.enums.RequestStatusEnum;
+import com.example.demo.dao.MovieConfigDao;
+import com.example.demo.dao.MovieDao;
+import com.example.demo.dto.MovieConfigDto;
+import com.example.demo.entity.Movie;
+import com.example.demo.entity.MovieConfig;
+import com.example.demo.exception.InvalidRequestParameterException;
+
+@Service
+public class MovieConfigService {
+	@Autowired
+	private MovieConfigDao movieConfigDao;
+
+	public RequestStatusEnum update(MovieConfig movieConfig) throws InvalidRequestParameterException {
+		if (!movieConfigDao.findByMovieId(movieConfig.getMovieId(), movieConfig.getBranchId()).isPresent()) {
+			return movieConfigDao.insert(movieConfig) == 1 ? RequestStatusEnum.SUCCESS : RequestStatusEnum.FAILURE;
+		} else {
+			return (movieConfigDao.update(movieConfig)) == 1 ? RequestStatusEnum.SUCCESS : RequestStatusEnum.FAILURE;
+		}
+	}
+
+	public List<MovieConfig> findAllByMovieId(String moviveId) throws InvalidRequestParameterException {
+		List<MovieConfig> list = movieConfigDao.findAllByMovieId(moviveId);
+		if (list.size() <= 0) {
+			throw new InvalidRequestParameterException("MovieConfig", RequestParameterEnum.NOT_FOUND);
+		}
+		return list;
+	}
+
+	public List<MovieConfigDto> FindAllMovieConfig() throws InvalidRequestParameterException {
+		List<MovieConfigDto> listMovie = movieConfigDao.findAll();
+		for (MovieConfigDto movieConfig : listMovie) {
+			List<MovieConfig> MovieConfigById = movieConfigDao.findAllByMovieId(movieConfig.getId());
+			movieConfig.setListMovieConfig(MovieConfigById);
+		}
+
+		return listMovie;
+	};
+}
