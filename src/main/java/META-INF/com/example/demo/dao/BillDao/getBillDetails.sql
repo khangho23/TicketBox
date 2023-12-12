0bbd11659c5861b26f12c203cd956a5a38c05bf7
khangho <hohoangkhang18@gmail.com>
2023-12-13 02:24:43 +0700

    getBillDetails

--- src/main/java/META-INF/com/example/demo/dao/BillDao/getBillDetails.sql
+++ src/main/java/META-INF/com/example/demo/dao/BillDao/getBillDetails.sql
@@ -1,88 +1,60 @@
-WITH TotalTicketPrice AS (
-    SELECT billid, SUM(totalprice) AS total_price
-    FROM ticket
-    WHERE billid = /* billId */139
-    GROUP BY billid
-)
-
-SELECT 
-    bill.id,
-    bill.exportdate,
-    bill.exportstatus,
-    bill.qrcode,
-    paymentmethod.name AS paymentmethod,
-    starttime,
-    showdate,
-    movie.poster,
-    movie.name AS movie,
-    movie.limitage,
-    movie.yearofmanufacture,
-    language.name AS language,
-    country.name AS country,
-    room.name AS room,
-    branch.name AS branch,
-    branch.address AS branch_address,
-    customer.name AS customer,
-    customer.phone AS customer_phone,
-    customer.email AS customer_email,
-    STRING_AGG(DISTINCT CONCAT(seat.rowseat, seat.orderseat), ', ') AS seats,
-    STRING_AGG(DISTINCT CONCAT(toppingdetails.quantity, topping.name), ', ') AS topping,
-    SUM(toppingdetails.pricewhenbuy) AS topping_totalprice,
-    TTP.total_price AS ticket_totalprice
-FROM 
-    bill
-LEFT JOIN 
-    paymentmethoddetails ON paymentmethoddetails.billid = bill.id
-LEFT JOIN 
-    paymentmethod ON paymentmethod.id = paymentmethoddetails.paymethodid
-JOIN 
-    ticket ON ticket.billid = bill.id
-JOIN 
-    customer ON customer.id = bill.customerid
-JOIN 
-    seatdetails ON ticket.seatdetailsid = seatdetails.id
-JOIN 
-    seat ON seat.id = seatdetails.seatid
-JOIN 
-    room ON room.id = seatdetails.roomid
-JOIN 
-    branch ON room.branchid = branch.id
-JOIN 
-    showtime ON showtime.id = ticket.showtimeid
-JOIN 
-    languageofmovie ON languageofmovie.id = showtime.languageofmovieid
-JOIN 
-    movie ON movie.id = languageofmovie.movieid
-JOIN 
-    "language" ON "language".id = languageofmovie.languageid
-JOIN 
-    country ON movie.countryid = country.id
-LEFT JOIN 
-    toppingdetails ON bill.id = toppingdetails.billid
-LEFT JOIN 
-    topping ON toppingdetails.billid = bill.id
-LEFT JOIN 
-    TotalTicketPrice TTP ON TTP.billid = bill.id
-WHERE 
-    ticket.billid = /* billId */139 AND customer.id = /* customerId */24
-GROUP BY 
-    bill.id,
-    bill.exportdate,
-    bill.exportstatus,
-    bill.qrcode,
-    paymentmethod.name,
-    starttime,
-    showdate,
-    movie.poster,
-    movie.name,
-    movie.limitage,
-    movie.yearofmanufacture,
-    language.name,
-    country.name,
-    room.name,
-    branch.name,
-    branch.address,
-    customer.name,
-    customer.phone,
-    customer.email,
-    TTP.total_price;
+SELECT distinct bill.id,
+       bill.exportdate,
+       bill.exportstatus,
+       bill.qrcode,
+       paymentmethod.name paymentmethod,
+       starttime,
+       showdate,
+	   movie.poster,
+       movie.name AS movie,
+       movie.limitage,
+       movie.yearofmanufacture,
+	   language.name AS language,
+       country.name AS country,
+       room.name AS room,
+       branch.name AS branch,
+       branch.address AS branch_address,
+       customer.name AS customer,
+       customer.phone AS customer_phone,
+       customer.email AS customer_email,
+       STRING_AGG(DISTINCT CONCAT(seat.rowseat, seat.orderseat), ', ') AS seats,
+       STRING_AGG(DISTINCT CONCAT(toppingdetails.quantity, topping.name), ', ') AS topping,
+       SUM(toppingdetails.pricewhenbuy) AS topping_totalprice,
+       SUM(ticket.totalprice) AS ticket_totalprice
+FROM bill
+         LEFT JOIN paymentmethoddetails ON paymentmethoddetails.billid = bill.id
+         LEFT JOIN paymentmethod ON paymentmethod.id = paymentmethoddetails.paymethodid
+         left JOIN ticket ON ticket.billid = bill.id
+         JOIN customer ON customer.id = bill.customerid
+         JOIN seatdetails ON ticket.seatdetailsid = seatdetails.id
+         JOIN seat ON seat.id = seatdetails.seatid
+         JOIN room ON room.id = seatdetails.roomid
+         JOIN branch ON room.branchid = branch.id
+         JOIN showtime ON showtime.id = ticket.showtimeid and showtime.roomid = room.id 
+         JOIN languageofmovie ON languageofmovie.id = showtime.languageofmovieid
+	 JOIN movie ON movie.id = languageofmovie.movieid
+	 left JOIN "language" ON "language".id = languageofmovie.languageid
+         JOIN country ON movie.countryid = country.id
+         LEFT JOIN toppingdetails ON bill.id = toppingdetails.billid
+         LEFT JOIN toppingofbranch  ON toppingdetails.toppingofbranchid = toppingofbranch.id 
+         left join topping on topping.id = toppingofbranch.toppingid 
+WHERE ticket.billid = /* billId */142 AND customer.id = /* customerId */24
+GROUP BY distinct bill.id,
+         bill.exportdate,
+         bill.exportstatus,
+        bill.qrcode,
+         paymentmethod.name,
+         starttime,
+         showdate,
+	 movie.poster,
+         movie.name,
+         movie.limitage,
+         movie.yearofmanufacture,
+	 language.name,
+         country.name,
+         room.name,
+         branch.name,
+         branch.address,
+         customer.name,
+         customer.phone,
+         customer.email;
